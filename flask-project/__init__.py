9f69ce61daf07916e73ed08842e560f0b849ee8e
DouleLove <sashazhur108@gmail.com>
2024-03-23 10:22:55 +0300

    create SERVER_HOST and SERVER_PORT constants

--- flask-project/__init__.py
+++ flask-project/__init__.py
@@ -7,4 +7,4 @@ from flask import Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['flask-app-secretkey']
 
-from .views import *  # NOQA  # force views to be initialized right after creating app
+from .views import *  # NOQA  # force views to be initialized before running server
--- flask-project/settings.py
+++ flask-project/settings.py
@@ -6,3 +6,6 @@ APPLICATION_PATH = os.path.join(BASE_PATH, 'flask-project')
 STATIC_PATH = os.path.join(APPLICATION_PATH, 'static')
 MEDIA_PATH = os.path.join(STATIC_PATH, 'img')
 DB_PATH = os.path.join(APPLICATION_PATH, 'database/db.sqlite3')
+
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 8000
