096b6c7784b83675b661b8b65e806b9ba0423965
PossessedCoder <dmitrijnikolae67@gmail.com>
2024-04-04 21:18:25 +0300

    blueprint

--- flask-project/__main__.py
+++ flask-project/__main__.py
@@ -1,8 +1,14 @@
-from . import app
-from .settings import SERVER_HOST, SERVER_PORT
+from flask import Flask, Blueprint
+import os
+import views
+from settings import SERVER_HOST, SERVER_PORT
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'penis'
 
 
 def run():
+    app.register_blueprint(views.blueprint)
     app.run(host=SERVER_HOST, port=SERVER_PORT)
 
 
