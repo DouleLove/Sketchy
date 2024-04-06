92f2d346488c74a840b97093d4cc71b37bf2667e
DouleLove <sashazhur108@gmail.com>
2024-04-06 11:24:50 +0300

    create forms.py

---
+++ Sketchy/forms.py
@@ -0,0 +1,13 @@
+__all__ = (
+    'LoginForm',
+)
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    login = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
