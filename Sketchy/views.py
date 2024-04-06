263504b2194b30bab198c2a382468e07c9d8887f
DouleLove <sashazhur108@gmail.com>
2024-04-06 11:27:06 +0300

    add user loader; add login_manager; add auth view

--- Sketchy/views.py
+++ Sketchy/views.py
@@ -1,9 +1,13 @@
+__all__ = ()
+
 import os
 from random import randint
 
 from flask import Blueprint, redirect, render_template, url_for, request
+from flask_login import LoginManager
 
-# from database import User, Session
+from database import User, Session
+from forms import LoginForm
 from settings import TEMPLATES_PATH, MEDIA_PATH
 
 blueprint = Blueprint(
@@ -11,6 +15,12 @@ blueprint = Blueprint(
     import_name=__name__,
     template_folder=TEMPLATES_PATH
 )
+login_manager = LoginManager()
+
+
+@login_manager.user_loader
+def load_user(uid):
+    return Session().query(User).get(uid)
 
 
 @blueprint.route('/')
@@ -23,7 +33,7 @@ def index():
     return render_template('index.html', previews=previews)
 
 
-@blueprint.route('/sketch/')
+@blueprint.route('/sketch')
 @blueprint.route('/sketch/<int:sketch_id>')
 def sketch_view(sketch_id=None):
     if not sketch_id:
@@ -33,6 +43,7 @@ def sketch_view(sketch_id=None):
     return str(sketch_id)  # render template here
 
 
-@blueprint.route('/auth/')
+@blueprint.route('/auth', methods=['GET', 'POST'])
 def auth():
-    return render_template('form.html', fields=[])
+    form = LoginForm()
+    return render_template('form.html', form=form)
