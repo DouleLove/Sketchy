278e74f9ce9261097276c0b68d11ff23b7041fa5
DouleLove <sashazhur108@gmail.com>
2024-04-09 21:40:04 +0300

    patrial-implementation of User model

--- Sketchy/database/_models.py
+++ Sketchy/database/_models.py
@@ -4,6 +4,7 @@ __all__ = (
 
 import sqlalchemy
 from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from . import BaseModel
 
@@ -16,4 +17,15 @@ class User(BaseModel, UserMixin):  # test model, should be removed or changed in
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     login = sqlalchemy.Column(sqlalchemy.String)
-    password = sqlalchemy.Column(sqlalchemy.String)
+    password_hash = sqlalchemy.Column(sqlalchemy.String)
+
+    @property
+    def password(self):
+        return self.password_hash
+
+    @password.setter
+    def password(self, value):
+        self.password_hash = generate_password_hash(value)
+
+    def check_password(self, data):
+        return check_password_hash(self.password, data)
