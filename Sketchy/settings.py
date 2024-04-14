e8ac294ed674b6d147af035b5095206f2e5a7794
DouleLove <sashazhur108@gmail.com>
2024-04-14 17:58:32 +0300

    add support for uploads

--- Sketchy/settings.py
+++ Sketchy/settings.py
@@ -6,15 +6,18 @@ APPLICATION_PATH = os.path.join(BASE_PATH, 'Sketchy')
 TEMPLATES_PATH = os.path.join(APPLICATION_PATH, 'templates')
 STATIC_PATH = os.path.join(APPLICATION_PATH, 'static')
 MEDIA_PATH = os.path.join(STATIC_PATH, 'img')
-DB_PATH = os.path.join(APPLICATION_PATH, 'database/db.sqlite3')
+UPLOAD_PATH = os.path.join(MEDIA_PATH, 'uploads')
+DB_PATH = os.path.join(APPLICATION_PATH, 'database/root/db.sqlite3')
 
 SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 8000
 DEBUG = False
 
 APP_CONFIG = {
-    'SECRET_KEY': os.environ.get('flask-app-secretkey', 'tmp-secretkey-should-be-replaced')
+    'SECRET_KEY': os.environ.get('flask-app-secretkey', 'tmp-secretkey-should-be-replaced'),
 }
+
+ALLOWED_MEDIA_EXTENSIONS = ['JPEG', 'JPG', 'PNG', 'GIF', 'TIFF']
 
 del os
 del Path
