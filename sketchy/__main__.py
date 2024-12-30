d4305073e20c0fbd4bf2f07e79235eea5ebd9c55
DouleLove <sashazhur108@gmail.com>
2024-12-30 19:54:58 +0300

    move application setup from runner file (__main__.py, was main.py previously) to new file - app.py

---
+++ sketchy/__main__.py
@@ -0,0 +1,21 @@
+__all__ = ()
+__title__ = "Sketchy"
+__version__ = "3.11"
+__license__ = "MIT"
+
+
+import sketchy.settings as settings
+from sketchy.app import Sketchy
+
+
+def run() -> None:
+    app = Sketchy(__name__)
+    app.run(
+        host=settings.SERVER_HOST,
+        port=settings.SERVER_PORT,
+        debug=settings.DEBUG,
+    )
+
+
+if __name__ == "__main__":
+    run()
---
+++ sketchy/app.py
@@ -0,0 +1,64 @@
+__all__ = ('Sketchy',)
+
+import runpy
+from typing import Any
+
+from flask import Flask, Response, g, send_from_directory
+from flask_login.login_manager import LoginManager
+from flask_wtf import CSRFProtect
+
+import sketchy.settings as settings
+from sketchy.database import Session, User
+from sketchy.utils import mkdirs_if_not_exist
+
+
+class Sketchy:
+
+    def __new__(cls, *args: Any, **kwargs: Any) -> Flask:
+        app = Flask(*args, **kwargs)
+        cls._setup(app)  # type: ignore
+        return app
+
+    @classmethod
+    def _setup(cls, app) -> None:
+        login_manager = LoginManager()
+        login_manager.user_loader(User.get)
+        login_manager.init_app(app)
+
+        CSRFProtect().init_app(app)
+
+        with app.app_context():
+            g.app = app
+
+            app.before_request(cls.before_request)
+            app.after_request(cls.after_request)
+
+            for module in settings.VIEWS_MODULES:
+                runpy.run_module(module)
+
+        app.config["SECRET_KEY"] = settings.SECRET_KEY
+        app.config["UPLOAD_FOLDER"] = settings.MEDIA_ROOT
+
+        app.get("/media/<path:filename>")(cls.media)
+
+        mkdirs_if_not_exist(
+            settings.DB_ROOT,
+            settings.MEDIA_ROOT
+        )
+
+    @classmethod
+    def before_request(cls) -> None:
+        g.session = Session()
+
+    @classmethod
+    def after_request(cls, response: Response) -> Response:
+        g.session.connection().close()
+        return response
+
+    @classmethod
+    def media(cls, filename: str) -> Response:
+        return send_from_directory(
+            settings.MEDIA_ROOT,
+            filename,
+            as_attachment=True,
+        )
