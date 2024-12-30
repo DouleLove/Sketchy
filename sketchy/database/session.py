35c37c9b2edb3cec1722deaa5ca1da5a8f5e2520
DouleLove <sashazhur108@gmail.com>
2024-12-30 19:49:30 +0300

    refactor imports

---
+++ sketchy/database/session.py
@@ -0,0 +1,26 @@
+__all__ = ("Session",)
+
+from typing import Any
+
+import sqlalchemy
+import sqlalchemy.orm as orm
+
+import sketchy.settings as settings
+
+from . import BaseModel
+
+engine = sqlalchemy.create_engine(
+    f"sqlite:///{settings.DB_PATH}?check_same_thread=False", echo=False
+)
+sessionmaker = orm.sessionmaker(bind=engine)
+
+# will be passed as metadata of BaseModel
+from . import _metadata  # noqa
+
+BaseModel.metadata.create_all(engine)
+
+
+class Session:
+
+    def __new__(cls, *args: Any, **kwargs: Any) -> orm.Session:
+        return sessionmaker()
