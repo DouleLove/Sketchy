f2551ab322364459318d590768f8e5258bf366f4
DouleLove <sashazhur108@gmail.com>
2024-12-30 19:48:51 +0300

    rename _sketch.py to sketch.py

---
+++ sketchy/database/sketch.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+__all__ = ("Sketch",)
+
+import sqlalchemy
+import sqlalchemy.orm as orm
+from flask import url_for
+
+from . import BaseModel
+
+
+class Sketch(BaseModel):
+    __tablename__ = "sketches"
+
+    id = sqlalchemy.Column(
+        sqlalchemy.Integer,
+        primary_key=True,
+        autoincrement=True,
+        unique=True,
+    )
+    name = sqlalchemy.Column(sqlalchemy.String(length=40))
+    place = sqlalchemy.Column(
+        sqlalchemy.String,
+        nullable=True,
+        default=None,
+    )
+    image_name = sqlalchemy.Column(sqlalchemy.String)
+    author_id = sqlalchemy.Column(
+        sqlalchemy.Integer,
+        sqlalchemy.ForeignKey("users.id"),
+    )
+    time_created = sqlalchemy.Column(sqlalchemy.DateTime)
+
+    author = orm.relationship("User")
+
+    @property
+    def image(self) -> str:
+        return url_for("media", filename=self.image_name)
+
+    @image.setter
+    def image(self, value: str) -> None:
+        self.image_name = value
