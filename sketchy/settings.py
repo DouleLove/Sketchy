f41af6c24701f7c52c128a69dadd824ce5b6f1f0
DouleLove <sashazhur108@gmail.com>
2024-12-30 19:53:44 +0300

    add VIEWS_MODULES constant, extracting yandex static apikey with gnvext

---
+++ sketchy/settings.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+import dotenv
+import gnvext
+
+dotenv.load_dotenv()
+
+BASE_PATH = Path(__file__).parent.parent
+APPLICATION_ROOT = BASE_PATH / "sketchy"
+TEMPLATES_ROOT = APPLICATION_ROOT / "templates"
+STATIC_ROOT = APPLICATION_ROOT / "static"
+IMG_ROOT = STATIC_ROOT / "img"
+MEDIA_ROOT = APPLICATION_ROOT / "media"
+DB_ROOT = APPLICATION_ROOT / "database" / "root"
+DB_PATH = DB_ROOT / "db.sqlite3"
+
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 8000
+DEBUG = False
+
+SECRET_KEY = gnvext.StringEnvVariable(
+    name="FLASK_APP_SECRETKEY",
+    default="tmp-secretkey-should-be-replaced",
+).value
+
+VIEWS_MODULES = [
+    "sketchy.views",
+]
+
+ALLOWED_MEDIA_EXTENSIONS = ("JPEG", "JPG", "PNG")
+
+APIKEY_GEOCODER = gnvext.StringEnvVariable("YANDEX_GEOCODER_APIKEY").value
+
+del Path
+del dotenv
+del gnvext
