89096d74eea6c5af341850d224d802a7f84d4bac
DouleLove <sashazhur108@gmail.com>
2025-01-07 23:37:07 +0300

    add user_by_login property to LoginForm

--- sketchy/forms/auth.py
+++ sketchy/forms/auth.py
@@ -1,6 +1,6 @@
 __all__ = ("LoginForm",)
 
-from functools import cache
+from functools import cached_property
 from typing import Any
 
 from flask_wtf import FlaskForm
@@ -33,33 +33,29 @@ class LoginForm(FlaskForm):
 
     def __init__(self, new: bool, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
+
         # boolean value which expected to be passed from auth view
         # (?n=true means new user (True) in url or "n" parameter is missing,
         # which means existing user (False))
         self._new = new
 
-    @cache
-    def _get_user(self):
+    @cached_property
+    def user_by_login(self):
         return User.get(login=self.login.data)
 
     def validate_login(self, _):
         if self._new:
-            if self._get_user():
+            if self.user_by_login:
                 raise ValidationError("Этот логин уже занят")
             return  # validation passed, ready to create new user
 
-        if not self._get_user():
+        if not self.user_by_login:
             raise ValidationError("Пользователь не найден")
 
     def validate_password(self, field):
         if (
             not self._new
-            and self._get_user()
-            and not self._get_user().check_password(field.data)
+            and self.user_by_login
+            and not self.user_by_login.check_password(field.data)
         ):
             raise ValidationError("Неверный пароль")
-
-    def validate_on_submit(self, extra_validators=None):
-        if super().validate_on_submit(extra_validators=extra_validators):
-            return self._get_user()
-        return False
