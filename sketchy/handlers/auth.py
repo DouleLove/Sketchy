d71f94c0239dc4612055aa2ac30e92225d9f8ab7
DouleLove <sashazhur108@gmail.com>
2025-01-07 23:37:57 +0300

    refactor: remove redundant auth_view_post_handler() user parameter, which is now can be obtained from the form.user_by_login

--- sketchy/handlers/auth.py
+++ sketchy/handlers/auth.py
@@ -10,34 +10,36 @@ from sketchy.database import User
 from sketchy.forms import LoginForm
 
 
-def auth_view_get_handler() -> Response | tuple[User | None, LoginForm]:
+def auth_view_get_handler() -> Response | LoginForm:
     if current_user.is_authenticated:
         return redirect("/profile")
 
     form = LoginForm(new=bool(request.args.get("n")))
 
-    # validation failed or form just created
-    if (user := form.validate_on_submit()) is False:
-        if request.args.get("n") is None:
-            form_template = "signin-form.html"
-        else:
-            form_template = "signup-form.html"
+    # validation succeed, pass form to auth_view_post_handler()
+    if form.validate_on_submit():
+        return form
 
-        return Response(
-            render_template(template_name_or_list=form_template, form=form)
-        )
+    if request.args.get("n") is None:
+        form_template = "signin-form.html"
+    else:
+        form_template = "signup-form.html"
 
-    return user, form
+    return Response(
+        render_template(template_name_or_list=form_template, form=form),
+    )
 
 
-def auth_view_post_handler(user: User, form: LoginForm) -> Response:
+def auth_view_post_handler(form: LoginForm) -> Response:
+    user = form.user_by_login
     if user is None:
-        session = g.session
         user = User()
         user.login = form.login.data
         user.password = form.password.data
-        session.add(user)
-        session.commit()
+        g.session.add(user)
+        g.session.commit()
+
     login_user(user, remember=True)
 
+    print(request.args.get('referrer'))
     return redirect(request.args.get("referrer", "/profile"))
