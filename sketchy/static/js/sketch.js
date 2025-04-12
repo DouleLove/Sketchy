20d3f44bb5a527ff888366aa62feec4c0d43b723
DouleLove <sashazhur108@gmail.com>
2025-04-12 14:56:36 +0300

    move script from sketch.html

--- sketchy/static/js/sketch.js
+++ sketchy/static/js/sketch.js
@@ -1,11 +1,42 @@
 import {addRendered} from './base.js';
 import {getURLParameters, formatURL, encodeURL} from './utils.js'
 
+function setSketchImage() {
+    const imageElement = document.getElementsByClassName('sketch-image-popup')[0];
+    console.log(imageElement);
+
+    let x = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+    let imageSize;
+    if (!x) {
+        imageSize = "large";
+    } else if (x <= 768) {
+        imageSize = "small";
+    } else if (x <= 1920) {
+        imageSize = "medium";
+    } else {
+        imageSize = "large";
+    }
+
+    if (imageElement.dataset.size == imageSize) {
+        return;
+    }
+
+    let imageSrc = imageElement.dataset.imname;
+    let mediaSplit = imageSrc.split('media/');
+    imageSrc = mediaSplit[0] + 'media/' + imageSize + '/' + mediaSplit[1];
+
+    imageElement.setAttribute('src', imageSrc);
+    imageElement.dataset.size = imageSize;
+}
 
 export function setupSketchPopUp() {
+    setSketchImage();
+    window.addEventListener('resize', setSketchImage);
+
     document.getElementById('btn-sketch-place').addEventListener('click', (e) => {
         window.location.href = encodeURL(
-            window.location.origin + `/sketches?query=${e.currentTarget.children[1].innerHTML}`
+            window.location.origin + `/imap?coordinates=${e.currentTarget.dataset.coordinates}`,
         );
     })
     const btnDeleteSketch = document.getElementById('form-delete-sketch')
