9640e05d6d9701ce17289ea33efa9b30d8d18f0e
DouleLove <sashazhur108@gmail.com>
2025-04-13 13:44:07 +0300

    implement api for getting sketches by outer and inner bounds

--- sketchy/handlers/imap.py
+++ sketchy/handlers/imap.py
@@ -1,14 +1,19 @@
 __all__ = ("imap_view_get_handler",)
 
-from flask import Response, render_template
+from http import HTTPStatus
+
+from flask import Response, g, render_template, jsonify, url_for
+from sqlalchemy import and_, or_
+
+from sketchy.database import Sketch
 
 
 def imap_view_get_handler(
     coordinates: list[float, float] = None,
-    top_left: list[float, float] = None,
-    bottom_right: list[float, float] = None,
+    outer_bounds: list[list[float, float], list[float, float]] = None,
+    inner_bounds: list[list[float, float], list[float, float]] = None,
 ) -> Response:
-    if not top_left or not bottom_right:
+    if not outer_bounds:
         return Response(
             render_template(
                 template_name_or_list="imap.html",
@@ -16,3 +21,38 @@ def imap_view_get_handler(
                 coordinates=coordinates,
             )
         )
+
+    # filtering sketches which coordinates are inside the outer bounds
+    query = g.session.query(Sketch).filter(
+        and_(
+            Sketch.longitude >= outer_bounds[0][0],
+            Sketch.longitude <= outer_bounds[1][0],
+            Sketch.latitude >= outer_bounds[0][1],
+            Sketch.latitude <= outer_bounds[1][1],
+        ),
+    )
+    if inner_bounds:
+        # filtering sketches which are inside the outer bounds,
+        # but outside the inner bounds
+        query = query.filter(
+            or_(
+                Sketch.longitude < inner_bounds[0][0],
+                Sketch.longitude > inner_bounds[1][0],
+                Sketch.latitude < inner_bounds[0][1],
+                Sketch.latitude > inner_bounds[1][1],
+            ),
+        )
+
+    data = []
+    for sketch in query.all():
+        data.append({
+            "sid": sketch.id,
+            "longitude": sketch.longitude,
+            "latitude": sketch.latitude,
+            'image': url_for("media", filename=f"tiny/{sketch.image_name}")
+        })
+
+    return jsonify(
+        status=HTTPStatus.OK,
+        data=data,
+    )
