b08add49d6c08151681fcf9c3341080f2ae4414c
DouleLove <sashazhur108@gmail.com>
2026-03-16 23:17:39 +0300

    fix map was opening in two windows on btnSketchPlace click

--- sketchy/static/js/sketch.js
+++ sketchy/static/js/sketch.js
@@ -47,9 +47,10 @@ function setupButtonSketchPlace() {
         );
         btnSketchPlace.style.fill = '#e67467';
         btnSketchPlace.dataset.title = 'Открыть Яндекс карты';
-        btnSketchPlace.addEventListener('click', () => {window.open(btnSketchPlaceRedirURL, '_blank');})
+        btnSketchPlace.addEventListener('click', () => {window.open(btnSketchPlaceRedirURL, '_blank');});
+    } else {
+        btnSketchPlace.addEventListener('click', () => {window.location.href = btnSketchPlaceRedirURL;});
     }
-    btnSketchPlace.addEventListener('click', () => {window.location.href = btnSketchPlaceRedirURL;})
 }
 
 
