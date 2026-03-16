9cf3b7b5c4199a2eebb67be6184f9d59b64fd87d
DouleLove <sashazhur108@gmail.com>
2026-03-16 23:18:05 +0300

    add local app run

--- sketchy/__main__.py
+++ sketchy/__main__.py
@@ -3,6 +3,10 @@ __title__ = "Sketchy"
 __version__ = "3.11"
 __license__ = "MIT"
 
+import sketchy.settings as settings
 from sketchy.app import Sketchy
 
 sketchy = Sketchy()
+
+if __name__ == '__main__':
+    sketchy.run(host=settings.HOST, port=settings.PORT)
